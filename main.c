ce8fc405d7bbf585a237845235d848f617ac363d
rocnen <adamek6256@gmail.com>
2025-03-08 23:16:51 +0100

    test

---
+++ main.c
@@ -0,0 +1,13 @@
+int iLoopCtr;
+int iPlusCounter;
+int iMinusCounter;
+int main()
+{
+	iPlusCounter=10;
+	iMinusCounter = 10;
+	for (iLoopCtr=0 ; iLoopCtr<5; iLoopCtr++)
+	{
+		iPlusCounter=iPlusCounter+3;
+		iMinusCounter=iMinusCounter-3;
+	}
+}
